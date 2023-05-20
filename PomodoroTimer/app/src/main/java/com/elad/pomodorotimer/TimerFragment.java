1d494b1b0ecdb7aab56ee936676a2be2909a0fe3
Elad Levi <60574244+eladlevi013@users.noreply.github.com>
2023-05-20 23:34:01 +0300

    functional pomodoro-timer

--- PomodoroTimer/app/src/main/java/com/elad/pomodorotimer/TimerFragment.java
+++ PomodoroTimer/app/src/main/java/com/elad/pomodorotimer/TimerFragment.java
@@ -1,64 +1,87 @@
 package com.elad.pomodorotimer;
 
 import android.os.Bundle;
-
+import android.os.CountDownTimer;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TimerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TimerFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView timer_tv;
+    Button start_timer_btn;
+    CountDownTimer countDownTimer;
+    boolean isTimerRunning = false;
+    long timeRemainingInMillis;
 
     public TimerFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TimerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TimerFragment newInstance(String param1, String param2) {
-        TimerFragment fragment = new TimerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static TimerFragment newInstance() {
+        return new TimerFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+        timer_tv = view.findViewById(R.id.timer_text);
+        start_timer_btn = view.findViewById(R.id.timer_toggle);
+
+        start_timer_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isTimerRunning) {
+                    stopTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(25 * 60 * 1000, 1000) { // 25 minutes (adjust as needed)
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeRemainingInMillis = millisUntilFinished;
+                updateTimerText();
+            }
+
+            @Override
+            public void onFinish() {
+                // Timer finished
+                timer_tv.setText("Timer Finished");
+                isTimerRunning = false;
+                start_timer_btn.setText("Start");
+            }
+        }.start();
+
+        isTimerRunning = true;
+        start_timer_btn.setText("Stop");
+    }
+
+    private void stopTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+
+        isTimerRunning = false;
+        start_timer_btn.setText("Start");
+        timeRemainingInMillis = 0;
+        updateTimerText();
+    }
+
+    private void updateTimerText() {
+        int minutes = (int) (timeRemainingInMillis / 1000) / 60;
+        int seconds = (int) (timeRemainingInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format("%02d:%02d", minutes, seconds);
+        timer_tv.setText(timeLeftFormatted);
     }
 }
